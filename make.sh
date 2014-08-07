408899e6853b9ae6924f8b84af4ccc9e77d5c783
Datafridge <feschmi.jena@gmail.com>
2014-08-07 21:36:52 +0200

    make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
-g++ main.cpp source/vector_field.cpp source/vector_field_generator.cpp -I include -o main -std=c++11 -l GL -Wall -pedantic
-
+g++ main.cpp source/vector_field.cpp source/vector_field_generator.cpp source/window.cpp -I include -I ../glfw/include -L ../glfw/src -o main -std=c++11 -l GL -Wall -pedantic -l glfw3
 ./main
